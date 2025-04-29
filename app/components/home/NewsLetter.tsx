263771e98810718909bdb9e7c150892473a7d1d5
Sumon Hossain <sumonengr.96@gmail.com>
2025-04-29 18:16:20 +0600

    WIP

--- app/components/home/NewsLetter.tsx
+++ app/components/home/NewsLetter.tsx
@@ -5,7 +5,7 @@ export default function NewsLetter() {
   return (
     <div className="w-full max-w-6xl mx-auto p-6 sm:p-10 bg-black text-white my-20 flex flex-col md:flex-row items-center justify-between rounded-3xl gap-10">
       <div className="w-full md:w-6/12 text-center md:text-left">
-        <p className="text-2xl sm:text-3xl mb-5 leading-snug">
+        <p className="text-2xl sm:text-4xl mb-5 leading-snug">
           সেরা শিক্ষকের তৈরি ক্লাস নোট এবং লেকচার সিট প্রয়োজন?
         </p>
         <button className="bg-primary text-white border border-primary px-6 py-2 rounded-full hover:bg-opacity-90 transition">
