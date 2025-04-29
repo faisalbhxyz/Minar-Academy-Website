d42dafb9bcc23a25654d2edc70fde9b4f7171d2b
Sumon Hossain <sumonengr.96@gmail.com>
2025-04-29 19:55:58 +0600

    WIP

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,12 +6,12 @@ import SelectClass from "./SelectClass";
 export default function Header() {
   return (
     <header className="flex items-center justify-between px-6 py-4 bg-white shadow">
-      <div className="w-1/2 text-xl font-bold text-gray-800">Minar</div>
+      <div className="md:w-1/2 text-xl font-bold text-gray-800">Minar</div>
       <div className="w-1/2 items-center justify-center gap-5 hidden md:flex">
         <SelectClass />
         <Menu />
       </div>
-      <div className="w-1/2 flex items-center justify-end gap-10">
+      <div className="md:w-1/2 flex items-center justify-end gap-3 md:gap-10">
         <button className="px-6 py-2 text-primary flex items-center gap-1">
           <BsFillTelephoneFill /> 16910
         </button>
