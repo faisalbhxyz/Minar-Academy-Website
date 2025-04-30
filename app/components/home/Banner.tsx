52fa69372600463c572cb3ac8b8e55b6ef21f04e
Sumon Hossain <sumonengr.96@gmail.com>
2025-04-30 19:09:39 +0600

    Banner

--- app/components/home/Banner.tsx
+++ app/components/home/Banner.tsx
@@ -12,7 +12,7 @@ const slides = ["/images/banner.jpg", "/images/banner-2.jpg"];
 
 export default function Banner() {
   return (
-    <div className="max-w-[1300px] mx-auto h-56 md:h-[500px] w-full overflow-hidden mt-5">
+    <div className="max-w-[1300px] mx-auto h-56 md:h-[500px] w-full overflow-hidden mt-5 rounded-2xl">
       <Swiper
         modules={[Autoplay, Pagination]}
         autoplay={{ delay: 3000 }}
@@ -27,7 +27,7 @@ export default function Banner() {
                 src={src}
                 alt={`Banner ${index + 1}`}
                 fill
-                className="object-cover rounded-2xl"
+                className="object-cover"
                 priority={index === 0}
                 sizes="100vw"
               />
--- app/components/home/CallToAction.tsx
+++ app/components/home/CallToAction.tsx
@@ -5,11 +5,11 @@ import { BsFillTelephoneFill } from "react-icons/bs";
 
 export default function CallToAction() {
   return (
-    <div className="absolute left-1/2 transform -translate-x-1/2 -bottom-[30rem] md:-bottom-32 w-11/12 md:w-9/12 bg-white shadow-lg p-3 rounded-2xl">
-      <div className="bg-primary/20 rounded-lg text-primary p-3 flex flex-col md:flex-row items-center justify-between gap-5">
-        <div>
+    <div className="absolute left-1/2 transform -translate-x-1/2 -bottom-[42rem] md:-bottom-32 w-11/12 md:w-9/12 bg-white shadow-lg p-3 rounded-2xl">
+      <div className="bg-primary/20 md:h-56 rounded-lg text-primary p-3 flex flex-col md:flex-row items-center justify-between gap-5">
+        <div className="">
           <p className="text-2xl mb-2">যেকোনো প্রয়োজনে কল করো এখনই</p>
-          <p className="text-sm w-5/12">যেকোনো জিজ্ঞাসায় কল করো</p>
+          <p className="text-sm">যেকোনো জিজ্ঞাসায় কল করো</p>
           <div className="border-l-4 border-orange-500 pl-2 my-5">
             <p>সকাল ১০ টা - রাত ১০ টা</p>
           </div>
@@ -20,14 +20,46 @@ export default function CallToAction() {
             <BsFillTelephoneFill /> 01886929763
           </Link>
         </div>
-        <div>
+        <div className="flex flex-col md:flex-row items-center gap-5 h-full">
           <Image
             src={"/images/minar-acedemy.jpg"}
             alt={"image"}
             width={300}
             height={300}
-            className="w-full md:w-48 h-72 md:h-48 rounded-xl"
+            className="w-full md:w-56 h-72 md:h-full rounded-xl"
           />
+          <div className="space-y-5 min-w-72">
+            <div className="flex items-center justify-between gap-5 bg-red-200 p-3 rounded-xl">
+              <div className="space-y-2">
+                <p>ফ্রি ভিডিও লাইব্রেরি</p>
+                <button className="bg-white px-4 py-2 rounded-md text-sm">
+                  ভিডিও দেখো
+                </button>
+              </div>
+              <Image
+                src={"/images/youtube.png"}
+                alt={"image"}
+                width={100}
+                height={100}
+                className="w-12 h-12"
+              />
+            </div>
+            <div className="flex items-center justify-between gap-5 bg-sky-200 p-3 rounded-xl">
+              <div className="space-y-2">
+                <p>মিনার একাডেমি ফেসবুক গ্রুপ</p>
+                <button className="bg-white px-4 py-2 rounded-md text-sm">
+                  গ্রুপে যুক্ত হও
+                </button>
+              </div>
+              <Image
+                src={"/images/facebook.png"}
+                alt={"image"}
+                width={100}
+                height={100}
+                className="w-12 h-12"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
